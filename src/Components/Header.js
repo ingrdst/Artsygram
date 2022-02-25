d9dedd873482a191f930f0ed79b4b07e21f2ae6e
Ingrid Soares <ingridgalllvao@gmail.com>
2022-02-25 05:23:30 -0300

    created header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 
 export const Header = () => {
   return (
-    <div className={styles.footer}>
-      <nav>
-        <Link to="/"> Home </Link>
-        <Link to="/login"> Login / Criar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="">
+          <Dogs />
+        </Link>
+        <Link className={styles.logo} to="/login">
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
